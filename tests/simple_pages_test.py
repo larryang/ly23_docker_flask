ebb5ae9d711d837e12b0d3598b8c0ae407ab0358
Larry Yang <2438671+larryang@users.noreply.github.com>
2022-02-27 19:47:43 -0500

    update unit tests for about and index, cleanup comments

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -1,7 +1,7 @@
 """This test the homepage"""
 
 def test_request_main_menu_links(client):
-    """This makes the index page"""
+    """This tests the navbar links"""
     response = client.get("/")
     assert response.status_code == 200
     assert b'<a class="nav-link" href="/about">About</a>' in response.data
@@ -11,42 +11,42 @@ def test_request_main_menu_links(client):
     assert b'<a class="nav-link" href="/page4">Page 4</a>' in response.data
 
 def test_request_index(client):
-    """This makes the index page"""
+    """This tests the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Index Page" in response.data
+    assert b"LY23 Bootstrap Index Webpage" in response.data
 
 def test_request_about(client):
-    """This makes the index page"""
+    """This tests the about page"""
     response = client.get("/about")
     assert response.status_code == 200
-    assert b"About Page" in response.data
+    assert b"About LY23 Page" in response.data
 
 def test_request_page1(client):
-    """This makes the index page"""
+    """This tests page1"""
     response = client.get("/page1")
     assert response.status_code == 200
     assert b"Page 1" in response.data
 
 def test_request_page2(client):
-    """This makes the index page"""
+    """This tests page2"""
     response = client.get("/page2")
     assert response.status_code == 200
     assert b"Page 2" in response.data
 
 def test_request_page3(client):
-    """This makes the index page"""
+    """This tests page3"""
     response = client.get("/page3")
     assert response.status_code == 200
     assert b"Page 3" in response.data
 
 def test_request_page4(client):
-    """This makes the index page"""
+    """This tests page 4"""
     response = client.get("/page4")
     assert response.status_code == 200
     assert b"Page 4" in response.data
 
 def test_request_page_not_found(client):
-    """This makes the index page"""
+    """This tests page 5"""
     response = client.get("/page5")
     assert response.status_code == 404
