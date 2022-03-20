b0c70f11c954089a0b169fc65df5b0723dcf7857
Larry Yang <2438671+larryang@users.noreply.github.com>
2022-03-19 20:03:55 -0400

    added unit test and implementation for Calculations.add_calculation() method, also added it for test fixture

--- calculator/history/__init__.py
+++ calculator/history/__init__.py
@@ -9,3 +9,9 @@ class Calculations:
     def count_history():
         """Return number of items in history"""
         return len(Calculations.history)
+
+    @staticmethod
+    def add_calculation(calculation):
+        """Add a Calculation to history and returns updated count"""
+        Calculations.history.append(calculation)
+        return Calculations.count_history()
--- tests/history_test.py
+++ tests/history_test.py
@@ -1,8 +1,24 @@
 """Testing Calculator History"""
-# import pytest
+import pytest
 from calculator.history import Calculations
+from calculator.calculation import Addition
+
+
+@pytest.fixture
+def add_addition_calculation_fixture():
+    """Add an Addition Calculation to history"""
+    values = (1.25, 2)
+    count = Calculations.count_history()
+    addition = Addition(values)
+    assert Calculations.add_calculation(addition) == (count + 1)
 
 
 def test_calculations_count():
     """Test history count"""
     assert Calculations.count_history() == 0
+
+
+def test_add_calculation(add_addition_calculation_fixture):
+    """Test adding addition"""
+    # pylint: disable=unused-argument,redefined-outer-name
+    assert Calculations.count_history() == 1
