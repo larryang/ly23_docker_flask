96f4581540ed47378f22aca4ce9110be73d6591e
Larry Yang <2438671+larryang@users.noreply.github.com>
2022-03-19 20:01:16 -0400

    reenable Calculator unit tests

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,22 +1,22 @@
-# """Testing the Calculator"""
-# from calculator import Calculator
+"""Testing the Calculator"""
+from calculator import Calculator
 
-# def arg_list():
-#     """ Arranging data for AAA testing"""
-#     # have a float and integer
-#     return 1.25, 4
-
-
-# def test_calculator_add_method():
-#     """Testing Calculator add function"""
-#     assert Calculator.add(arg_list()) == 5.25
+def arg_list():
+    """ Arranging data for AAA testing"""
+    # have a float and integer
+    return 1.25, 4
 
 
-# def test_calculator_subtract_method():
-#     """Testing Calculator subtract function"""
-#     assert Calculator.subtract(arg_list()) == -5.25
+def test_calculator_add_method():
+    """Testing Calculator add function"""
+    assert Calculator.add(arg_list()) == 5.25
 
 
-# def test_calculator_multiply_method():
-#     """Testing Calculator multiply function"""
-#     assert Calculator.multiply(arg_list()) == 5
+def test_calculator_subtract_method():
+    """Testing Calculator subtract function"""
+    assert Calculator.subtract(arg_list()) == -5.25
+
+
+def test_calculator_multiply_method():
+    """Testing Calculator multiply function"""
+    assert Calculator.multiply(arg_list()) == 5
